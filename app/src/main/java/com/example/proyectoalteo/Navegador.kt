3a6bb610396d5f78c473b8e95fe1b523c42eb7cb
Totkin <82641427+Totkin@users.noreply.github.com>
2021-11-22 23:12:11 +0100

    Primer commit

--- app/src/main/java/com/example/proyectoalteo/Navegador.kt
+++ app/src/main/java/com/example/proyectoalteo/Navegador.kt
@@ -15,8 +15,8 @@ class Navegador : AppCompatActivity() {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_navegador)
 
-        val miVisorWeb: WebView = findViewById(R.id.visor)      //inicializamos el visor WEB
-        miVisorWeb.webViewClient = object : WebViewClient(){    //le diremos que use elcliente de Internet nativo del Dispositivo
+        val miVisorWeb: WebView = findViewById(R.id.visor)      //inicializamos el visor WebView
+        miVisorWeb.webViewClient = object : WebViewClient(){    //le diremos que use el cliente de Internet nativo del Dispositivo
             override fun shouldOverrideUrlLoading(
                 view: WebView?,
                 url: String                                     //Cambio la solicitud por una URL que sera String
@@ -26,10 +26,10 @@ class Navegador : AppCompatActivity() {
             }
         }
         miVisorWeb.loadUrl("https://google.com")            //direccoin donde vamos a  buscar
-        miVisorWeb.settings.javaScriptEnabled=true              //configuramos para que no tenga limites al cargar el sitio WEB
+        miVisorWeb.settings.javaScriptEnabled=true
         miVisorWeb.settings.allowContentAccess=true
         miVisorWeb.settings.domStorageEnabled=true
-        miVisorWeb.settings.useWideViewPort=true
+        miVisorWeb.settings.useWideViewPort=true                   //configuramos para que no tenga limites al cargar el sitio WEB
         miVisorWeb.settings.setAppCacheEnabled(true)            //el guardado de cache esta absoleto para esta API, pero aun lo pondre
     }
 }
